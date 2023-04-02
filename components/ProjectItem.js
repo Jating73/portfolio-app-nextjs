610aa3f17f2b03d364285ca5ce3cec79549d2cc0
Jatin Gambhir <jatin.gambhir@jungleworks.com>
2023-04-02 20:01:59 +0530

    fixed projectItem

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -4,7 +4,7 @@ import { Card } from 'react-bootstrap';
 function ProjectItem({ project }) {
     return (
         <Card>
-            <a href={project.url} target="_blank">
+            <a href={project.url} target="_blank" rel="noreferrer">
                 <Card.Img variant="top" src={project.screenshot_url} height="200px" style={{ objectFit: "cover" }} />
             </a>
         </Card>
